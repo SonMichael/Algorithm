7090849030413a0788da8791e2455d0d53d995ff
sonle <sonle@les-MacBook-Pro.local>
2022-05-29 10:28:51 +0700

    comment for distinct subsets

--- distinct_subsets.py
+++ distinct_subsets.py
@@ -14,11 +14,27 @@
 
 def subsets(nums):
     n = len(nums)
+    # 2 ^ n mean: we get all cases:
+    # For ex:
+    # [1] => n = 1 => 2 ^ n = 2 => [[], [1]]
+    # [1, 2] => n = 2 => 2 ^ n = 4 => [[], [1], [2], [1,2]]
+    # bits is present for number so
+    # if number can't duplicate we will have bits can't duplicate and we only choose with bit = 1 because
+    # we only have value at bit = 1 position and
+    # if we choose with bit = 0 we will have duplicate subsets
     numSubsets = 2 ** n
     result = []
     for i in range(0 , numSubsets):
         items = []
         for j in range(0 , n):
+            # we only care the last bit
+            # For ex:
+            # 5 = 101 => (101 >> 0 & 1) = 1
+            # 5 = 101 => (101 >> 2 & 1) = 1
+            # Because 2 ^ n is total case and we only select with bit = 1 
+            # so we only need shift n steps, we can cover all case
+            # if we shift more than n we only receive with result = 0
+            # if we shift less than n we can't select all cases
             if (i >> j) & 1:
                 items.append(nums[j])
         result.append(items)
