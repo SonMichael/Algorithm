84772f84db2ee8fb77a60649e30ef82a6dfbacbd
sonle <sonle@les-MacBook-Pro.local>
2022-07-03 10:10:37 +0700

    change comment for minimum_in_rotated_sorted

--- minimum_in_rotated_sorted_array.py
+++ minimum_in_rotated_sorted_array.py
@@ -1,7 +1,7 @@
 # Suppose an array sorted in ascending order is rotated at
 # some pivot unknown to you beforehand
 # (ie, [0,1,2,4,5,6,7] might become
-# [4,5,6,7,6,1,2]).
+# [4,5,6,7,0,1,2]).
 # Find the minimum element.
 # You may assume no duplicate exists in the array.
 # Example 1
