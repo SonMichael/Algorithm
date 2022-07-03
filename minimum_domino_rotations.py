d9b1ebcf2f2fb8c3060c4af9cea605b53bcff16b
sonle <sonle@les-MacBook-Pro.local>
2022-07-03 09:11:16 +0700

    change logic minmum rotation

--- minimum_domino_rotations.py
+++ minimum_domino_rotations.py
@@ -3,36 +3,32 @@
 # We may rotate the i^th domino, so that tops[i] and bottoms[i] swap values.
 # Return the minimum number of rotations so that all the values in tops are the same, or all the
 # values in bottoms are the same.
-# Ex: tops = [2,1,2,4,2,2], 
-#  bottoms = [5,2,6,2,3,2]
-# result: 
-# tops =    [2,2,2,2,2,2]
-# bottoms = [5,1,6,4,3,2]
+# Ex: tops = [1,2,1,4,1,1]
+#  bottoms = [5,1,6,1,3,2]
+# result: 2
+# 2 <-> 1 , 4 <-> 1 => 2
+
+# Ex2: tops = [1,2,1,1,1,1]
+#  bottoms =  [5,1,6,1,3,2]
+# result: 1
+#  2 <-> 1 => 1
+
 def minDominoRotations(tops, bottoms):
     if len(tops) == 0 or len(bottoms) == 0:
         return -1
-    hash = dict()
-    for i in range(0 , len(tops)):
-        make_hash(hash, i, tops[i])
-        make_hash(hash, i, bottoms[i])
-    listHash = list(hash)
-    key0, key1 = listHash[0], listHash[1]
-    count0, _ = hash.get(key0)
-    if count0 >= len(tops) -1:
-        return hash.get(key0)
-    return hash.get(key1)
-    
-    
-def make_hash(hash, i, key):
-    if hash.get(key) == None:
-        arr = 6 * [0]
-        arr[i] = key
-        hash[key] = [1, arr]
-        return
-    count, poses = hash.get(key)
-    poses[i] = key
-    hash[key] = [count + 1, poses]
 
-tops =    [2,1,1,3,2,2]
-bottoms = [1,2,1,1,1,1]
+    for target in [tops[0], bottoms[0]]:
+        missingT , missingB = 0, 0
+        for i, pair in enumerate(zip(tops, bottoms)):
+            top, bottom = pair
+            if  target != top and target  != bottom:
+                break
+            if top != target: missingT += 1
+            elif bottom != target: missingB += 1
+            if i == len(tops) - 1:
+                return min(missingT, missingB)
+    return -1
+    
+tops =    [1,2,1,4,1,1]
+bottoms = [5,1,6,1,3,2]
 print(minDominoRotations(tops, bottoms))
