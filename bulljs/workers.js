e1ef3d62b893e20b54f2e9b16a082d458ed3d16c
sonle <sonle@les-MacBook-Pro.local>
2022-10-21 05:42:54 +0700

    add redis to bulljs

--- bulljs/workers.js
+++ bulljs/workers.js
@@ -1,12 +1,21 @@
 import Queue from "bull";
 import process from 'process'
 import EventEmitter from "events";
+import Redis from "ioredis"
+
 const eventEmitter = new EventEmitter();
 
 const queue = new Queue('my-first-queue');
+const redis = new Redis(6379, "127.0.0.1")
+const KEY_TEST_CONCURRENCY_DATA = 'test_concurrency_data'
 
-const main = () => {
+const initRedisData = async () => {
+  await redis.set(KEY_TEST_CONCURRENCY_DATA, 2)
+}
 
+
+const main = async () => {
+  await initRedisData()
   const users = [
     {id:  1,  name: "name1", age: 31 },
     { id: 2, name: "name2", age: 25 },
@@ -33,14 +42,29 @@ const main = () => {
   
   queue.add({ name: "name6", age: 30 });
   
-  const actionFirstQueue = (job, done) => {
+  
+
+  const actionPromise = (time) => {
+    
+    return new Promise((resolve, reject) => 
+      setTimeout(async () => {
+        let redisData = await redis.get(KEY_TEST_CONCURRENCY_DATA)
+        await redis.set(KEY_TEST_CONCURRENCY_DATA, redisData - 1)
+        resolve({
+          redisData
+        })
+      }, time))
+  }
+
+  const actionFirstQueue = async (job, done) => {
     console.log('1', process.pid,job.data);
+    let { redisData} = await actionPromise(100)
+    console.log("🚀 ~ file: workers.js ~ line 56 ~ actionFirstQueue ~ redisData",  redisData)
     done();
   }
 
-  // queue1 and queue2 can't run the same time, it's mean when queue1 handle "name1" that it's done
-  // const queue1 = new Queue('my-first-queue');
-  queue.process('__default__', 4, actionFirstQueue);
+  // we can change number of worker to test concurrency
+  queue.process('__default__', 3, actionFirstQueue);
   
   queue.on('completed', function (job, result) {
     console.log("completed")
